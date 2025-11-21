840de49037156d6e2cb0cb9e434b4927df855cb8
Soad Hossain <shossain3@pride.hofstra.edu>
2025-11-21 16:05:07 -0500

    Added more setup details

--- src/Adoption.java
+++ src/Adoption.java
@@ -1,2 +1,40 @@
 public class Adoption {
+    int date;
+    Animal animal;
+    Person person;
+
+    public Adoption(int date, Animal animal, Person person) {
+        this.date = date;
+        this.animal = animal;
+        this.person = person;
+    }
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return date + ": " + animal.toString() + " adopted by " + person.toString();
+
+    }
 }
